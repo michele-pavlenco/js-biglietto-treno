f2ab33459d9292bf58082a403142a9c0bba6d613
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-03-24 17:34:56 +0100

    added-html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 // alert ("caio")
+let userName = (prompt ("Qualè il tuo nome ?"));
+console.log(userName);
+
+let userSurname = (prompt ("Qualè il tuo cognome?"));
+console.log(userSurname);
+
 let numberKm = prompt( " Qunti chilometri vuoi percorrere oggi? ");
 console.log ( "numero chilometri:" , numberKm);
 
@@ -8,11 +14,28 @@ console.log ( "Età:" , age);
 const price4Km = "0.21"
 console.log (price4Km)
  
-let priceTicket = numberKm * price4Km 
-console.log ( "prezzo:", priceTicket  )
+let price = numberKm * price4Km 
+console.log ( "prezzo:", price  )
+let scontoMinori
+let scontoover65
 
-if (age < 18, priceTicket * 0.20) {
-    console.log("prezzo per minorenni" , )
-      
-  
- }
+if (age < 18 ) {
+     price = price - (price * 0.20)  
+    console.log("Prezzo minori:", price)
+  console.log(age < 18)
+ }else if(age > 65 ){
+ price = price - (price * 0.40);
+    console.log("Prezzo over 65:", price);
+  console.log(age > 65);
+ } else{ price = price
+    }
+
+ document.getElementById("nome").innerHTML += userName 
+
+ document.getElementById("cognome").innerHTML += userSurname
+
+ document.getElementById("numeroKm").innerHTML += numberKm
+
+  document.getElementById("price").innerHTML  += Math.round(100*price)/100
+
+
