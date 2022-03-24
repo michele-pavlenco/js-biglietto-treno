94cf6e33c8a56b48387636b69f5eb3d2c7d85698
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-03-24 15:57:57 +0100

    added-price-js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// alert ("caio")
+let numberKm = prompt( " Qunti chilometri vuoi percorrere oggi? ");
+console.log ( "numero chilometri:" , numberKm);
+
+let age = prompt("Quanti anni hai?")
+console.log ( "Età:" , age);
+
+const price4Km = "0.21"
+console.log (price4Km)
+ 
+let priceTicket = numberKm * price4Km 
+console.log ( "prezzo:", priceTicket  )
+
+if (age < 18, priceTicket * 0.20) {
+    console.log("prezzo per minorenni" , )
+      
+  
+ }
